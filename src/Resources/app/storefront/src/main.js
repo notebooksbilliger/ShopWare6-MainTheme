967ddde5f207800b1f14dc00a464b7ac8dbab3b0
Maciej Dejewski <mdejewski@notebooksbilliger.de>
2020-05-08 01:11:30 +0200

    [NPS-642] Backup. GLightbox and all files work through webpack and npm

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,7 +1,9 @@
+console.log('MainTheme -> main.js', '111');
+
 // Import custom js modules
 import './js/enableSubmitAfterPageLoad';
 
-// Import all necessary 3rd party libraries
-
 // Globals definition
-global.GLightbox = require('../../../../../node_modules/glightbox');
+global.GLightbox = require('../../../../../node_modules/glightbox/');
+
+console.log('after require()');
