90b4cbbf12b91dfa160c2127c7f636910323ce9c
Maciej Dejewski <mdejewski@notebooksbilliger.de>
2020-05-11 12:36:37 +0200

    [NPS-642] extracts constants to seperate module, refactors .js

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,9 +1,3 @@
-console.log('MainTheme -> main.js', '111');
-
-// Import custom js modules
 import './js/enableSubmitAfterPageLoad';
 
-// Globals definition
 global.GLightbox = require('../../../../../node_modules/glightbox/');
-
-console.log('after require()');
